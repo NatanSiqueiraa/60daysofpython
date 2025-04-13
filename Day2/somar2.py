ec416fcb25c1a6bd943990335da075b7ec468a63
NatanSiqueiraa <sisten353535@gmail.com>
2025-04-13 16:10:43 -0300

    adicionando o dia 2

---
+++ Day2/somar2.py
@@ -0,0 +1,12 @@
+def somar():
+    n1 = int(input("Digite o primeiro número: "))
+    n2 = int(input("Digite o segundo número: "))
+
+    resultado = n1 + n2
+
+    print(f"A soma entre {n1}+{n2}={resultado}")
+    return
+
+somar()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 60 dias de progamacao em python
 - [x] Dia 1 Criando o meu primeiro código em python
+- [x] Dia 2 Somando números de duas formas distintas
+
+
